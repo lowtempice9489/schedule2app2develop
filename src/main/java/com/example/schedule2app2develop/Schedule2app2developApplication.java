b8281cb325eed67b0b1f49e545273f7720e1e6c4
b7749abc92-boop <b7749abc92@gmail.com>
2026-07-08 18:38:55 +0900

    일정 관리 앱 Develop 과제 제작 시작(필수기준)

---
+++ src/main/java/com/example/schedule2app2develop/Schedule2app2developApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule2app2develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schedule2app2developApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schedule2app2developApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/schedule2app2develop/schedule/entity/Schedule.java
@@ -0,0 +1,19 @@
+package com.example.schedule2app2develop.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String author;
+}
