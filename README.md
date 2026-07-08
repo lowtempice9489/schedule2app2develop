f307002fbeafc29ff1f6c095ad2f686e262e6b1b
b7749abc92-boop <b7749abc92@gmail.com>
2026-07-08 18:38:55 +0900

    일정 관리 앱 Develop 과제 제작 시작

---
+++ README.md
@@ -0,0 +1,3 @@
+## ERD
+
+![erd]![bjJLRTR5dXFHenNhUGh4eTdfUG9nQmd3.png](bjJLRTR5dXFHenNhUGh4eTdfUG9nQmd3.png)
---
+++ src/test/java/com/example/schedule2app2develop/Schedule2app2developApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule2app2develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schedule2app2developApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
