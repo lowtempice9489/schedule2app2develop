77fbb966da9e05cf3b570ff0976bdb33c8dd5606
b7749abc92-boop <b7749abc92@gmail.com>
2026-07-08 19:14:56 +0900

    일정 관리 앱 Develop 과제 제작 시작(필수기준)

---
+++ src/main/java/com/example/schedule2app2develop/schedule/threelayer/ScheduleService.java
@@ -0,0 +1,13 @@
+package com.example.schedule2app2develop.schedule.threelayer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+
+    private final ScheduleRepository sdRepository;
+
+
+}
